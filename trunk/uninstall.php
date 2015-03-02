4cef9e82b484f2af69a78fa59aadbcdf5ce873cf
stephen.r.rider@gmail.com <stephen.r.rider@gmail.com@9bab96ec-131a-11de-bc8f-6fc8e0c7d88e>
2015-03-02 01:12:41 +0000

    testing

--- trunk/uninstall.php
+++ trunk/uninstall.php
@@ -4,7 +4,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit();	// sanity check
 
 function uninstall_strider_core_plugin() {
-	//	TODO:	needs an uninstall (not deactivate) function that removes current plugin from "plugin exists" array.  If array is then empty (no other SC plugins, whether active or not), delete strider_core_data and strider_core_options
+	//	TODO: needs an uninstall (not deactivate) function that removes current plugin from "plugin exists" array.  If array is then empty (no other SC plugins, whether active or not), delete strider_core_data and strider_core_options
 }
 
 uninstall_strider_core_plugin();
